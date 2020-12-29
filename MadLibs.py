717883e39ef813828f1123409a0782de85f8684f
Humbleone786 <hassanazam41@gmail.com>
2020-12-29 15:57:52 -0500

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,22 @@
+loop=1
+while (loop < 2):
+	noun = input("Choose a Noun (plural if necessary): ")
+	noun1 = input("Choose another Noun: ")
+	noun2 = input("Choose another Noun: ")
+	noun3 = input("Choose another Noun: ")
+	p_noun = input("Choose a Pronoun: ")
+	verb = input("Choose a Verb (past tense): ")
+	organization = input("Choose an Organization: ")
+	organization1 = input("Choose another Organization: ")
+	organization2 = input("Choose another Organization: ")
+	profession = input("Choose a Profession: ")
+	number = input("Choose a Number: ")
+	country = input("Choose a Country: ")
+	
+	print("")
+	print("What the fuck did you just fucking say about me, you little bitch?")
+	print("I'll have you know I", verb, "top of my class in the", organization, "and I've been involved in numerous secret raids on", organization1, "and I have over", number, "of confirmed", noun, ".")
+	print("I am trained in", noun1, noun2, "and I'm the top", profession,"in the entire", country, organization2, ".")
+	print("You are", p_noun, "to me but just another", noun3, ".")
+	
+	loop = loop + 1
